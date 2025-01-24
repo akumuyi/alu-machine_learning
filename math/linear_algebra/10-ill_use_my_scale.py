47e22796456eb44aa39829d115ef30d461a79f2f
akumuyi <a.kumuyi@alustudent.com>
2025-01-24 16:10:12 +0200

    2. Size Me Please

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" defines function that calculates the shape of a matrix using numpy """
+
+
+def np_shape(matrix):
+    """ calculates the shape of a matrix using numpy.ndarray """
+    return matrix.shape
+    
