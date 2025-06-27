f31f40cdcd5bad5166853629faf529a33dda3479
akumuyi <a.kumuyi@alustudent.com>
2025-06-27 23:31:37 +0200

    34. Log stats

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
 """
-Nginx logs stored in MongoDB:
+Nginx logs stored in MongoDB statistics
 """
 
 from pymongo import MongoClient
+import sys
+
+def main():
+    """Main function to retrieve and display log statistics"""
+    try:
+        # Connect to MongoDB
+        client = MongoClient('mongodb://127.0.0.1:27017', serverSelectionTimeoutMS=2000)
+        db = client.logs
+        collection = db.nginx
+        
+        # Get total logs
+        total_logs = collection.count_documents({})
+        
+        # Get method counts
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        method_counts = {method: collection.count_documents({"method": method}) for method in methods}
+        
+        # Get status check count
+        status_check_count = collection.count_documents(
+            {"method": "GET", "path": "/status"}
+        )
+        
+        # Print results
+        print("{} logs".format(total_logs))
+        print("Methods:")
+        for method in methods:
+            print("\tmethod {}: {}".format(method, method_counts[method]))
+        print("{} status check".format(status_check_count))
+        
+    except Exception as e:
+        print("Error: {}".format(e), file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
-
-    # Get the total number of documents
-    total_logs = collection.count_documents({})
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {}
-    for method in methods:
-        method_counts[method] = collection.count_documents({"method": method})
-
-    # Get the count of status check
-    status_check_count = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-
-    # Print the stats in Python 3.5 compatible format
-    print("{} logs".format(total_logs))
-    print("Methods:")
-    for method in methods:
-        print("\tmethod {}: {}".format(method, method_counts[method]))
-    print("{} status check".format(status_check_count))
+    main()
